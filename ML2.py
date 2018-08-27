a03bed083aa79a1462fe6b0fbe550532eafdf26b
Megha <meghabhatt1996@gmail.com>
2018-08-27 11:30:48 +0530

    Add files via upload
    Some more practice of Python

---
+++ ML2.py
@@ -0,0 +1,23 @@
+def func1():
+    var1 = 12
+    v2 = 3
+    v3 = var1+v2
+    print(v3)
+#func1()
+
+def func2(var1,var2):
+    var3= var1+var2
+    print(var3)
+#func2(2,3)    
+
+def func3():
+    var3 = 12+5
+    return var3
+#var4 = func3()
+#print(var4)
+
+def func4(var1,v2):
+    v3 = var1+v2
+    return v3
+#v5 = func4(4,5)
+#print(v5)
